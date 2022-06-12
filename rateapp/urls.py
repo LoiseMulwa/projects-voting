7842fe2e4a280c73118c1771603721e1cc8bb5c1
LoiseMulwa <loisemulwa7@gmail.com>
2022-06-13 01:05:06 +0300

    ft:Add urls for login and register links

--- rateapp/urls.py
+++ rateapp/urls.py
@@ -6,13 +6,13 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-   url(r'^$',views.login_view, name='login'),
+  url(r'^$',views.login_view, name='login'),
   url(r'^register/',views.register_view, name='register'),
-  url(r'^index/',views.index,name = 'index'),
-  url(r'^search/', views.search_results, name='search_results'),
-  url(r'^new/project$', views.new_project, name='new-project'),
-  url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
-  url(r'^accounts/edit/', views.edit_profile, name='edit_profile'),
+  # url(r'^index/',views.index,name = 'index'),
+  # url(r'^search/', views.search_results, name='search_results'),
+  # url(r'^new/project$', views.new_project, name='new-project'),
+  # url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+  # url(r'^accounts/edit/', views.edit_profile, name='edit_profile'),
 ]
 
 if settings.DEBUG:
