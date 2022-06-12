dea38a936e72e3a8d37cc7d9e2e86df9cd5c1e44
LoiseMulwa <loisemulwa7@gmail.com>
2022-06-12 18:57:16 +0300

    ft: Added database password and user

--- rate/settings.py
+++ rate/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'rate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'gallery',
-#         'USER': 'moringa',
-#         'PASSWORD':'1234',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+        'PASSWORD':'1234',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
