18a41c754b570d4ea59e9ff4ab81cf765f39183c
LoiseMulwa <loisemulwa7@gmail.com>
2022-06-13 00:13:45 +0300

    ft: Create profile class in models

--- rateapp/models.py
+++ rateapp/models.py
@@ -21,27 +21,27 @@ class Profile(models.Model):
     profile = Profile.objects.filter(user = id).first()
     return 
 
-# class Project(models.Model):
-#   profile = models.ForeignKey(User, related_name="projects", on_delete=models.CASCADE)
-#   title = models.CharField(max_length=144)
-#   description = models.TextField()
-#   img = ImageField(manual_crop = '1920x1080')
-#   live_site = models.URLField(max_length=250)
+class Project(models.Model):
+  profile = models.ForeignKey(User, related_name="projects", on_delete=models.CASCADE)
+  title = models.CharField(max_length=144)
+  description = models.TextField()
+  img = ImageField(manual_crop = '1920x1080')
+  live_site = models.URLField(max_length=250)
 
-#   @classmethod
-#   def get_projects(cls):
-#     projects = cls.objects.all()
-#     return projects
+  @classmethod
+  def get_projects(cls):
+    projects = cls.objects.all()
+    return projects
 
-#   @classmethod
-#   def search_by_title(cls,search_term):
-#     projects = cls.objects.filter(title__icontains=search_term)
-#     return projects
+  @classmethod
+  def search_by_title(cls,search_term):
+    projects = cls.objects.filter(title__icontains=search_term)
+    return projects
 
-#   @classmethod
-#   def get_profile_projects(cls, profile):
-#     projects = Project.objects.filter(profile__pk = profile)
-#     return projects
+  @classmethod
+  def get_profile_projects(cls, profile):
+    projects = Project.objects.filter(profile__pk = profile)
+    return projects
 
 # class Review(models.Model):
 #   design = models.IntegerField(default=0)
