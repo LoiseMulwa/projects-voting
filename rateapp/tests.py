2827e462c1b3d9cd6c4c18d3fc70536b6f4cd84b
LoiseMulwa <loisemulwa7@gmail.com>
2022-06-13 00:53:52 +0300

    ft:Tests for review class

--- rateapp/tests.py
+++ rateapp/tests.py
@@ -32,17 +32,17 @@ class ProfileTestClass(TestCase):
     profile = Profile(bio="sometext")
     self.assertEqual(str(profile), 'sometext')
 
-# class ReviewTestClass(TestCase):
+class ReviewTestClass(TestCase):
 
-#   # Set up method
-#   def setUp(self):
-#     self.rev= Review(bio = 'Titl1', prof_pic = 'profpic.png' )
+  # Set up method
+  def setUp(self):
+    self.rev= Review(bio = 'Titl1', prof_pic = 'profpic.png' )
 
-#   # Testing  instance
-#   def test_instance(self):
-#     self.assertTrue(isinstance(self.prof,Profile))
+  # Testing  instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.prof,Profile))
 
-#   # Testing String representation Method
-#   def test_string_representation(self):
-#     profile = Profile(bio="sometext")
-#     self.assertEqual(str(profile), 'sometext')
+  # Testing String representation Method
+  def test_string_representation(self):
+    profile = Profile(bio="sometext")
+    self.assertEqual(str(profile), 'sometext')
