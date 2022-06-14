bedb12f29e1a7b4afb2d1405e140539e7b8d3fc2
LoiseMulwa <loisemulwa7@gmail.com>
2022-06-14 17:21:50 +0300

    ft:Link main js file

--- rateapp/static/scripts/main.js
+++ rateapp/static/scripts/main.js
@@ -5,3 +5,10 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+$(document).ready(function(){
+    $('form').submit(function(event){
+      event.preventDefault()
+    }) // End of submit event
+  
+  }) // End of document ready function
